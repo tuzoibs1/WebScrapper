017b4775219f94afba9d4ff8977d99449c087faa
tuzoibs1 <tuzoibsa@gmail.com>
2019-11-20 00:00:21 -0600

    modified

--- craig_scrapper/my_app/urls.py
+++ craig_scrapper/my_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home0'),
+    path('new_search', views.new_search, name='new_search'),
 ]
 
 
--- craig_scrapper/my_app/views.py
+++ craig_scrapper/my_app/views.py
@@ -1,5 +1,16 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search') 
+    print(search)
+    stuff_for_frontend ={
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
+
+    
