6a1cd6270a662fdcafeb4fcc3f1801ca676f9eea
tuzoibs1 <tuzoibsa@gmail.com>
2019-11-26 22:07:55 -0600

    m

--- craig_scrapper/my_app/views.py
+++ craig_scrapper/my_app/views.py
@@ -5,7 +5,7 @@ from requests.compat import quote_plus
 from . import models
 
 #url with quiry thats dynamically generated
-BASE_CRAIGLIST_URL =  'https://minneapolis.craigslist.org/search/?query={}'
+BASE_CRAIGSLIST_URL =  'https://minneapolis.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg' # actually stored in data pid as data-ids 
 
 def home(request):
@@ -14,18 +14,19 @@ def home(request):
 def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)     #creating search object to hold searched querys in db
-    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search)) #qoute pluse alows ther to be empth spaces in search
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))#qoute pluse alows ther to be empth spaces in search
     response = requests.get(final_url) #print(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser') #parse data to bs4 object 
 
-    post_listings = soup.find_all('li', {'class':'result-row'})
+    post_listings = soup.find_all('li', {'class': 'result-row'})
 
     final_postings = []
 
     for post in post_listings:
-        post_title = post.find(class_='result-title').text 
+        post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
+
             
         if post.find(class_='result-price'): #for searching that dont have price
             post_price = post.find(class_='result-price').text
